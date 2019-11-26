04ac490736cc51b267e513266762cb777a6e4f16
confoundedkyle <56545560+confoundedkyle@users.noreply.github.com>
2019-11-26 11:14:57 -0700

    Initial Commit

---
+++ 15 - Packages/import_demo.py
@@ -0,0 +1,11 @@
+# No parameter set
+import helpers
+helpers.display('Sample message')
+
+# Parameter set to True
+import helpers
+helpers.display('Sample message', True)
+
+# Importing the display function from helpers module
+from helpers import display
+display('Sample message')
