625a45fe7376ddab1240f0a64d53f40958099150
confoundedkyle <27352794+confoundedkyle@users.noreply.github.com>
2019-11-26 07:32:38 -0700

    Initial Commit

---
+++ 15 - Packages/installing_packages.py
@@ -0,0 +1,14 @@
+# Install an individual package
+pip install colorama
+
+# Install from a list of packages
+pip install -r requirements.txt
+
+# requirements.txt
+colorama
+
+
+# To install a specific python package version /
+# whether it is the first time, an upgrade or a downgrade use:
+
+pip install --force-reinstall MySQL_python==1.2.4
---
+++ 15 - Packages/virtual_environment_activate.py
@@ -0,0 +1,14 @@
+# windows systems
+
+# cmd.exe
+<folder_name>\Scripts\Activate.bat
+
+# Powershell
+<folder_name>\Scripts\Activate.ps1
+
+# bash shell
+. ./<folder_name>/Scripts/Activate
+
+
+# OSX/Linux (bash)
+<folder_name>/bin/activate
